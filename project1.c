e1974630b6abe6f4215a7e2080e4db7ac9623e0a
mzhang4 <jimmyzms5@gmail.com>
2014-09-18 17:03:21 -0500

    communucate the matrix mutiple with specific input

--- project1.c
+++ project1.c
@@ -1,12 +1,13 @@
 #include <pthread.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 #define M 3
 #define K 2
 #define N 3
 
-int A [M] [K] = { {1,4j, {2,5j, {3,6j }; 
-int B [K] [N] = { {8,7,6j, {5,4,3j }; 
+int A [M] [K] = { {1,4}, {2,5}, {3,6} }; 
+int B [K] [N] = { {8,7,6}, {5,4,3} }; 
 int C [M] [N];
 
 struct pos
@@ -17,7 +18,27 @@ struct pos
 
 void* calculateIJ(void *param)
 {
+	struct pos *p = (struct pos*)(param);
+	int ii;
+	for(ii=0;ii<K;ii++)
+		C[p->i][p->j] += A[p->i][ii]*B[ii][p->j];
 
+	pthread_exit(0);
+
+}
+
+void output()
+{
+	int i,j;
+	for(i=0;i<M;i++)
+	{
+		for(j=0;j<N;j++)
+		{
+			printf("%d ",C[i][j]);
+
+		}
+		printf("\n");
+	}
 }
 
 int main()
@@ -26,5 +47,19 @@ int main()
 	pthread_attr_t attr;
 
 	pthread_attr_init(&attr);
-	
+	int i, j;
+	for(i=0;i<M;i++)
+		for(j=0;j<N;j++)
+		{
+			struct pos *p = (struct pos*) malloc(sizeof(struct pos));
+			p->i=i;
+			p->j=j;
+			
+			pthread_attr_init(&attr);
+			pthread_create(&tid,&attr,calculateIJ, (void*)p);
+			pthread_join(tid,NULL);
+		}
+	output();
+	return 0;
+
 }
